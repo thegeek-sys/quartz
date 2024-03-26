5f88944bc9964796797bd9a3ff415e44edc57f45
thegeek-sys <55345633+thegeek-sys@users.noreply.github.com>
2024-03-26 17:29:13 +0100

    possible fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Vault, Flavio Sperandeo",
+    pageTitle: "🪴 Quartz 4.0",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "it-IT",
-    baseUrl: "jackyzha0.github.io/quartz",
+    locale: "en-US",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
